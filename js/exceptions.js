d45e36c2ad022fb385799d9cc41dcbde91765c3e
rasteron <rasteron@live.com>
2017-01-23 11:33:59 +0800

    update minified script and use client-build

--- js/exceptions.js
+++ js/exceptions.js
@@ -1,14 +1,1 @@
-
-define(function() {
-
-    var Exceptions = {
-
-        LootException: Class.extend({
-            init: function(message) {
-                this.message = message;
-            }
-        })
-    };
-
-    return Exceptions;
-});
+define(function(){var Exceptions={LootException:Class.extend({init:function(message){this.message=message}})};return Exceptions});
